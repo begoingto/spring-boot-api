cf10c202b171c3973d1a522bd0758e9e667ddfa9
Begoingto <begoingto.me@gmail.com>
2023-05-11 10:11:23 +0700

    Customize SecurityConfig  ✅🆗

---
+++ src/main/java/com/begoingto/springbootapi/security/SecurityConfig.java
@@ -0,0 +1,79 @@
+package com.begoingto.springbootapi.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder encoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    // Define in-memory user
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetailsManager userDetailsManager = new InMemoryUserDetailsManager();
+        UserDetails admin = User.builder()
+                .username("admin")
+//                .password("{noop}123")
+                .password(encoder().encode("123"))
+                .roles("ADMIN")
+                .build();
+        UserDetails goldUser = User.builder()
+                .username("gold")
+                .password(encoder().encode("123"))
+                //.password("{noop}123")
+                .roles("ACCOUNT")
+                .build();
+        UserDetails user = User.builder()
+                .username("user")
+                .password(encoder().encode("123"))
+                //.password("{noop}123")
+                .roles("USER")
+                .build();
+        userDetailsManager.createUser(admin);
+        userDetailsManager.createUser(user);
+        userDetailsManager.createUser(goldUser);
+        return userDetailsManager;
+    }
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.csrf(AbstractHttpConfigurer::disable);
+//        http.csrf(token -> token.disable());
+
+        http.authorizeHttpRequests(request -> {
+            //Authorize URL mapping
+            request.requestMatchers("/api/v1/users/**").hasRole("ADMIN");
+            request.requestMatchers("/api/v1/account-types/**","/api/v1/files/**").hasAnyRole("ACCOUNT","USER");
+            request.anyRequest().permitAll();
+        });
+
+
+        //Security mechanism
+        http.httpBasic();
+
+
+        http.sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS); // make security stateless
+//                .formLog/in(); // using for normal web without api
+        return http.build();
+    }
+
+}
