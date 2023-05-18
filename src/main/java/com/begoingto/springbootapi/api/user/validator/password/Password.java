20a95b09befb8abcf35e25766cf0fb481ef23678
Begoingto <begoingto.me@gmail.com>
2023-05-18 09:38:05 +0700

    Password Strong validation✅🆗

--- src/main/java/com/begoingto/springbootapi/api/user/validator/password/Password.java
+++ src/main/java/com/begoingto/springbootapi/api/user/validator/password/Password.java
@@ -1,10 +1,8 @@
 package com.begoingto.springbootapi.api.user.validator.password;
 
 
-import com.begoingto.springbootapi.api.user.validator.email.EmailUniqueConstraintValidator;
 import jakarta.validation.Constraint;
 import jakarta.validation.Payload;
-
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
--- src/main/java/com/begoingto/springbootapi/api/user/validator/role/RoleIdConstraintValidator.java
+++ src/main/java/com/begoingto/springbootapi/api/user/validator/role/RoleIdConstraintValidator.java
@@ -5,15 +5,17 @@ import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 import lombok.RequiredArgsConstructor;
 
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @RequiredArgsConstructor
-public class RoleIdConstraintValidator implements ConstraintValidator<RoleIdConstraint, Set<Integer>> {
+public class RoleIdConstraintValidator implements ConstraintValidator<RoleIdConstraint, List<Integer>> {
     private final UserMapper userMapper;
     @Override
-    public boolean isValid(Set<Integer> roleIds, ConstraintValidatorContext context) {
-
-        for (Integer id: roleIds){
+    public boolean isValid(List<Integer> roleIds, ConstraintValidatorContext context) {
+        Set<Integer> UniqueId = new HashSet<>(roleIds);
+        for (Integer id: UniqueId){
             if (!userMapper.checkRoleId(id)) return false;
         }
 
